e0a4e7bfc871a942bce0400b631cdaeda8a8d88f
OlyaZasimovich <olechka-zosya@mail.ru>
2018-03-02 23:57:59 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	var counter = 0;
+	for (var i=1; i<=number; i++) {
+		var b = i;
+		while(b >= 5 && b % 5 === 0) {
+			b = b / 5;
+			counter++;
+		};
+	};
+
+	return counter;
 }
